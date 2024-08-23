d6e8477e562f2b4c5d9a3686eaa96393e46f8516
Mohak-Royal <mohak.royal2112@gmail.com>
2024-08-23 17:44:40 +0530

    Scanning

---
+++ Basic/Scanning.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Scanning {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter the value in INT: ");
+        // int num = sc.nextInt();
+
+        // System.out.println("Enter the value in float: ");
+        // float f = sc.nextFloat();
+        // System.out.println("The value of INT is : " + num);
+        // System.out.println("The value of Float is: " + f);
+
+        // System.out.println("Enter the string: ");
+        // // String str = sc.next();  // it will only scan one word
+        // String str = sc.nextLine();
+
+        // System.out.println("The String is : " + str);
+
+
+        // System.out.println("Enter the boolean: ");
+        // boolean bool = sc.nextBoolean();
+
+        // System.out.println("The boolean is: " + bool);
+
+
+        // System.out.println("Enter the byte: ");
+        // byte b = sc.nextByte();
+
+
+        // System.out.println("The byte is: " + b);
+
+    }    
+}
